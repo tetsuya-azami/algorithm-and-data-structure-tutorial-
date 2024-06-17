1099e2e3bb547b907bd92da60cbe6a7fea34e096
tetsuya-azami <zqianjian35@gmail.com>
2024-06-17 09:29:04 +0900

    implement binarySearch

---
+++ src/test/kotlin/Chapter1Test.kt
@@ -0,0 +1,12 @@
+import java.io.ByteArrayInputStream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Chapter1Test {
+    @Test
+    fun `chapter1-1`() {
+        System.setIn(ByteArrayInputStream("28".toByteArray()))
+        val result = Chapter1.numberOfTimesToFindAnswer()
+        assertEquals(4, result)
+    }
+}
