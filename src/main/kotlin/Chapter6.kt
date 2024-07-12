ce4a1884ac42bd751dabba2c13eb189d706cdf8e
tetsuya-azami <zqianjian35@gmail.com>
2024-07-12 16:32:40 +0900

    solve balloonPenalty

--- src/main/kotlin/Chapter6.kt
+++ src/main/kotlin/Chapter6.kt
@@ -4,7 +4,7 @@ class Chapter6 {
             var left = 0
             var right = sortedList.size - 1
             var result = false
-            do {
+            while (left <= right) {
                 val mid = (left + right) / 2
                 if (sortedList[mid] == target) {
                     result = true
@@ -12,9 +12,59 @@ class Chapter6 {
                 }
                 if (sortedList[mid] < target) left = mid + 1
                 if (sortedList[mid] > target) right = mid - 1
-            } while (left <= right)
+            }
 
             return result
+        }
+
+        fun findMinimumSumMoreThanOrEqualToK(firstList: List<Int>, secondList: List<Int>, k: Int): Int {
+            val sortedSecondList = secondList.sorted()
+            var result = Int.MAX_VALUE
+            for (i in firstList.indices) {
+                var left = 0
+                var right = sortedSecondList.size - 1
+                while (right >= left) {
+                    val mid = (right + left) / 2
+                    if (firstList[i] + secondList[mid] >= k) {
+                        result = minOf(result, firstList[i] + secondList[mid])
+                        right = mid - 1
+                    } else {
+                        left = mid + 1
+                    }
+                }
+            }
+
+            return result
+        }
+
+        fun balloonPenalty(heights: List<Int>, speeds: List<Int>): Int {
+            // ペナルティの上限値
+            val maxPenalty = heights.mapIndexed { index, height ->
+                height + speeds[index] * heights.size
+            }.maxBy { it }
+
+            var left = 0
+            var right = maxPenalty
+
+            while (right > left + 1) {
+                var mid = (left + right) / 2
+                var isOk = true
+                val timeLimits = MutableList(heights.size) { 0 } // 制限時間
+
+                for (i in heights.indices) {
+                    if (mid < heights[i]) isOk = false
+                    else timeLimits[i] = (mid - heights[i]) / speeds[i]
+                }
+
+                val sortedTimeLimits = timeLimits.sorted()
+                for (i in heights.indices) {
+                    if (sortedTimeLimits[i] < i) isOk = false
+                }
+                if (isOk) right = mid
+                else left = mid + 1
+            }
+
+            return right
         }
     }
 }
--- src/test/kotlin/Chapter6Test.kt
+++ src/test/kotlin/Chapter6Test.kt
@@ -9,4 +9,27 @@ class Chapter6Test {
         println()
         println(result)
     }
+
+    @Test
+    fun findMinimumSumMoreThanOrEqualToK() {
+        val result = Chapter6.findMinimumSumMoreThanOrEqualToK(
+            firstList = listOf(2, 3, 4, 4),
+            secondList = listOf(4, 5, 7, 7),
+            k = 7
+        )
+        println()
+        println()
+        println()
+        println(result)
+    }
+
+    @Test
+    fun balloonPenalty() {
+        val result =
+            Chapter6.balloonPenalty(heights = listOf(2, 3, 4, 4), speeds = listOf(5, 2, 2, 2)) // 22, 11, 12, 12
+        println()
+        println()
+        println()
+        println(result)
+    }
 }
