103c54bac90b5163b4acb1f9d5888d219689f4b0
tetsuya-azami <zqianjian35@gmail.com>
2024-07-17 08:18:31 +0900

    implement Stack

---
+++ src/main/kotlin/Stack.kt
@@ -0,0 +1,34 @@
+class Stack<T> {
+    companion object {
+        private val MAX = 100
+    }
+
+    private var head: Int = 0
+    private val elements: MutableList<T> = mutableListOf()
+
+    fun isEmpty(): Boolean {
+        return head == 0
+    }
+
+    fun isFull(): Boolean {
+        return head == MAX
+    }
+
+    fun push(element: T) {
+        if (isFull()) {
+            throw RuntimeException("is Full")
+        }
+        elements.add(head, element)
+        head++
+    }
+
+    fun pop(): T {
+        if (isEmpty()) {
+            throw RuntimeException("is Empty")
+        }
+        val result = elements[head - 1]
+        head--
+
+        return result
+    }
+}
---
+++ src/test/kotlin/StackTest.kt
@@ -0,0 +1,27 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class StackTest {
+    @Test
+    fun test() {
+        val stack = Stack<Int>()
+        // 最初isEmptyであること
+        assertTrue { stack.isEmpty() }
+
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        assertEquals(3, stack.pop())
+        assertEquals(2, stack.pop())
+        assertEquals(1, stack.pop())
+        assertTrue { stack.isEmpty() }
+    }
+
+    @Test
+    fun isFullTest() {
+        val stack = Stack<Int>()
+        (1..100).forEach { stack.push(it) }
+        assertTrue { stack.isFull() }
+    }
+}
