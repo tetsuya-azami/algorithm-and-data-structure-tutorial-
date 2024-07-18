ab98890a56f30ea2dc7f254da9ca2a49863c5727
tetsuya-azami <zqianjian35@gmail.com>
2024-07-19 07:08:46 +0900

    refactor reversePolishNotation

--- src/main/kotlin/Chapter9.kt
+++ src/main/kotlin/Chapter9.kt
@@ -1,27 +1,25 @@
-import java.util.Stack
-
 class Chapter9 {
     companion object {
         fun reversePolishNotation(mathematicalFormula: String): Int {
             val elements = mathematicalFormula.split(" ")
-            val stack = Stack<Int>()
+            val stack = ArrayDeque<Int>()
             for (element in elements) {
                 if (element.length == 1 && element[0] in '0'..'9') {
-                    stack.push(element[0] - '0')
+                    stack.add(element[0] - '0')
                 } else {
-                    val first = stack.pop()
-                    val second = stack.pop()
+                    val first = stack.removeLast()
+                    val second = stack.removeLast()
                     when (element) {
-                        "+" -> stack.push(second + first)
-                        "-" -> stack.push(second - first)
-                        "*" -> stack.push(second * first)
-                        "/" -> stack.push(second / first)
+                        "+" -> stack.add(second + first)
+                        "-" -> stack.add(second - first)
+                        "*" -> stack.add(second * first)
+                        "/" -> stack.add(second / first)
                         else -> throw ArithmeticException("存在しない演算子です。$element")
                     }
                 }
             }
 
-            return stack.pop()
+            return stack.removeLast()
         }
     }
 }
