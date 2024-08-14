03ff9ef36e30ae88a6e1da5c3fe3b2bc043b0803
tetsuya-azami <zqianjian35@gmail.com>
2024-08-15 06:34:41 +0900

    solve KthLargest

---
+++ src/main/java/neetcode/heapprioritykey/KthLargest.java
@@ -0,0 +1,29 @@
+package neetcode.heapprioritykey;
+
+import java.util.PriorityQueue;
+
+class KthLargest {
+
+    private int k;
+    private PriorityQueue<Integer> queue;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.queue = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            queue.offer(nums[i]);
+            if (k < queue.size()) {
+                queue.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        queue.offer(val);
+        if (k < queue.size()) {
+            queue.poll();
+        }
+
+        return queue.peek();
+    }
+}
---
+++ src/test/java/neetcode/heapprioritykey/KthLargestTest.java
@@ -0,0 +1,22 @@
+package neetcode.heapprioritykey;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KthLargestTest {
+    @Test
+    public void test() {
+        KthLargest kthLargest = new KthLargest(3, new int[]{1, 2, 3, 3});
+        int result1 = kthLargest.add(3);
+        assertEquals(3, result1);
+        int result2 = kthLargest.add(5);
+        assertEquals(3, result2);
+        int result3 = kthLargest.add(6);
+        assertEquals(3, result3);
+        int result4 = kthLargest.add(7);
+        assertEquals(5, result4);
+        int result5 = kthLargest.add(8);
+        assertEquals(6, result5);
+    }
+}
