c763ed89fe5665649ce076975d1ca51c9340cf8e
tetsuya-azami <zqianjian35@gmail.com>
2024-08-19 09:38:42 +0900

    solve minCostClimbingStairs

--- src/main/java/neetcode/heappriorityqueue/Solution.java
+++ src/main/java/neetcode/heappriorityqueue/Solution.java
@@ -29,7 +29,15 @@ public class Solution {
             b = a;
             a = tmp;
         }
-        
+
         return a;
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        }
+
+        return Math.min(cost[0], cost[1]);
     }
 }
