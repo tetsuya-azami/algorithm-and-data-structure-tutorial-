fe48ba35e7ffb735790631aea5a279b652aeca94
tetsuya-azami <zqianjian35@gmail.com>
2024-08-31 10:15:41 +0900

    solve invertTree

---
+++ src/main/java/neetcode/trees/SolutionRe.java
@@ -0,0 +1,13 @@
+package neetcode.trees;
+
+public class SolutionRe {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
---
+++ src/test/java/neetcode/trees/SolutionReTest.java
@@ -0,0 +1,26 @@
+package neetcode.trees;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionReTest {
+    @Test
+    public void invertTree() {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(2, node4, node5);
+        TreeNode node3 = new TreeNode(3, node6, node7);
+        TreeNode root = new TreeNode(1, node2, node3);
+        SolutionRe solutionRe = new SolutionRe();
+        TreeNode result = solutionRe.invertTree(root);
+        printTreeNode(result);
+    }
+
+    private void printTreeNode(TreeNode treeNode) {
+        if (treeNode == null) return;
+        System.out.println(treeNode.val);
+        printTreeNode(treeNode.left);
+        printTreeNode(treeNode.right);
+    }
+}
