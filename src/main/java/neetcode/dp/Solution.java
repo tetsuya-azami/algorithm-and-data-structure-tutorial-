0112f830ca7e711eacecb7d9780cc15ee804a2f5
tetsuya-azami <zqianjian35@gmail.com>
2024-09-05 08:06:48 +0900

    solve minCostClimbingStairs

--- src/main/java/neetcode/dp/Solution.java
+++ src/main/java/neetcode/dp/Solution.java
@@ -1,6 +1,7 @@
 package neetcode.dp;
 
 public class Solution {
+
     public int climbStairs(int n) {
         int[] array = new int[n + 1];
         array[n] = 1;
@@ -11,5 +12,16 @@ public class Solution {
         }
 
         return array[0];
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i], dp[i - 1] + cost[i]);
+        }
+
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
 }
--- src/test/java/neetcode/dp/SolutionTest.java
+++ src/test/java/neetcode/dp/SolutionTest.java
@@ -12,4 +12,14 @@ class SolutionTest {
         System.out.println();
         System.out.println(result);
     }
+
+    @Test
+    public void minCostClimbingStairs() {
+        Solution solution = new Solution();
+        int result = solution.minCostClimbingStairs(new int[]{1, 2, 1, 2, 1, 1, 1});
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(result);
+    }
 }
