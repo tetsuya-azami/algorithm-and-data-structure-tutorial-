1b381ee03762633226d0f087ab5153afd97a99ac
tetsuya-azami <zqianjian35@gmail.com>
2024-09-05 08:16:40 +0900

    solve minCostClimbingStairsModelAnswer

--- src/main/java/neetcode/dp/Solution.java
+++ src/main/java/neetcode/dp/Solution.java
@@ -24,4 +24,11 @@ public class Solution {
 
         return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
+
+    public int minCostClimbingStairsModelAnswer(int[] cost) {
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] = Math.min(cost[i] + cost[i + 1], cost[i] + cost[i + 2]);
+        }
+        return Math.min(cost[0], cost[1]);
+    }
 }
