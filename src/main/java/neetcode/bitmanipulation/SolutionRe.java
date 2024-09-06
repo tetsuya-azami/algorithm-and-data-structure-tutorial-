394ddc1469a2b5cac096a9e60d32a2fce9103c1d
tetsuya-azami <zqianjian35@gmail.com>
2024-09-07 08:51:51 +0900

    solve singleNumber

---
+++ src/main/java/neetcode/bitmanipulation/SolutionRe.java
@@ -0,0 +1,11 @@
+package neetcode.bitmanipulation;
+
+public class SolutionRe {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/neetcode/bitmanipulation/SolutionReTest.java
@@ -0,0 +1,16 @@
+package neetcode.bitmanipulation;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionReTest {
+
+    @Test
+    public void singleNumber() {
+        SolutionRe solutionRe = new SolutionRe();
+        int result = solutionRe.singleNumber(new int[]{3, 2, 3});
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(result);
+    }
+}
