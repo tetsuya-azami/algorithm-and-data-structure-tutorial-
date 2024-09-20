abb82fe32384fd89f0fefda23d2d2c317ac06185
tetsuya-azami <zqianjian35@gmail.com>
2024-09-21 08:14:09 +0900

    solve dailyTemperatures

--- src/main/java/neetcode/stack/Solution.java
+++ src/main/java/neetcode/stack/Solution.java
@@ -84,4 +84,19 @@ public class Solution {
             stack.pop();
         }
     }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] results = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    results[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return results;
+    }
 }
--- src/test/java/neetcode/stack/SolutionTest.java
+++ src/test/java/neetcode/stack/SolutionTest.java
@@ -2,6 +2,7 @@ package neetcode.stack;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 class SolutionTest {
@@ -34,5 +35,15 @@ class SolutionTest {
         System.out.println();
         System.out.println();
         System.out.println(result);
+    }
+
+    @Test
+    public void dailyTemperatures() {
+        Solution solution = new Solution();
+        int[] result = solution.dailyTemperatures(new int[]{30, 38, 30, 36, 35, 40, 28});
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(Arrays.toString(result));
     }
 }
