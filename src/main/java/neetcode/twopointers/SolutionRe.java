fbd9dbbc4dfbc675093408d4bc6967979479e997
tetsuya-azami <zqianjian35@gmail.com>
2024-11-02 18:56:06 +0900

    solve threeSumHashMap

--- src/main/java/neetcode/twopointers/SolutionRe.java
+++ src/main/java/neetcode/twopointers/SolutionRe.java
@@ -1,5 +1,7 @@
 package neetcode.twopointers;
 
+import java.util.*;
+
 public class SolutionRe {
     public int[] twoSumBruteForce(int[] numbers, int target) {
         for (int i = 0; i < numbers.length; i++) {
@@ -29,5 +31,32 @@ public class SolutionRe {
         }
 
         return new int[0];
+    }
+
+    public List<List<Integer>> threeSumHashMap(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            count.put(nums[i], count.get(nums[i]) - 1);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) - 1);
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int target = -(nums[i] + nums[j]);
+                if (count.getOrDefault(target, 0) > 0)
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], target)));
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.getOrDefault(nums[j], 0) + 1);
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/neetcode/twopointers/SolutionReTest.java
+++ src/test/java/neetcode/twopointers/SolutionReTest.java
@@ -3,6 +3,7 @@ package neetcode.twopointers;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 class SolutionReTest {
     @Test
@@ -23,5 +24,15 @@ class SolutionReTest {
         System.out.println();
         System.out.println();
         System.out.println(Arrays.toString(result));
+    }
+
+    @Test
+    void threeSumHashMap() {
+        SolutionRe solutionRe = new SolutionRe();
+        List<List<Integer>> result = solutionRe.threeSumHashMap(new int[]{-1, 0, 1, 2, -1, -4});
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(result);
     }
 }
