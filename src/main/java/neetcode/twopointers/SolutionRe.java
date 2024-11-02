e0b45ab2de8560ed252dc784683a766ac4a668e7
tetsuya-azami <zqianjian35@gmail.com>
2024-11-02 17:47:06 +0900

    solve twoSumTwoPointers

--- src/main/java/neetcode/twopointers/SolutionRe.java
+++ src/main/java/neetcode/twopointers/SolutionRe.java
@@ -14,4 +14,20 @@ public class SolutionRe {
 
         return new int[]{};
     }
+
+    public int[] twoSumTwoPointers(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) return new int[]{left + 1, right + 1};
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[0];
+    }
 }
--- src/test/java/neetcode/twopointers/SolutionReTest.java
+++ src/test/java/neetcode/twopointers/SolutionReTest.java
@@ -6,9 +6,19 @@ import java.util.Arrays;
 
 class SolutionReTest {
     @Test
-    void twoSum() {
+    void twoSumBruteForce() {
         SolutionRe solutionRe = new SolutionRe();
-        int[] result = solutionRe.twoSum(new int[]{1, 2, 3, 4}, 3);
+        int[] result = solutionRe.twoSumBruteForce(new int[]{1, 2, 3, 4}, 3);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(Arrays.toString(result));
+    }
+
+    @Test
+    void twoSumTwoPointers() {
+        SolutionRe solutionRe = new SolutionRe();
+        int[] result = solutionRe.twoSumTwoPointers(new int[]{1, 2, 3, 4}, 3);
         System.out.println();
         System.out.println();
         System.out.println();
