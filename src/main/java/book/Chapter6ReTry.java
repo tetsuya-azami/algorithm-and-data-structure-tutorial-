eda43edcc221013cdfd6ee4bf2e0f07ca355f11f
tetsuya-azami <zqianjian35@gmail.com>
2024-11-10 08:36:41 +0900

    solve coordinateCompressionRe

--- src/main/java/book/Chapter6ReTry.java
+++ src/main/java/book/Chapter6ReTry.java
@@ -1,5 +1,6 @@
 package book;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,6 +16,31 @@ public class Chapter6ReTry {
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             result[entry.getValue()] = i;
             i++;
+        }
+
+        return result;
+    }
+
+    public int[] coordinateCompressionRe(int[] target) {
+        int[] result = new int[target.length];
+        int[] sortedTarget = Arrays.copyOf(target, target.length);
+        Arrays.sort(sortedTarget);
+
+        for (int i = 0; i < target.length; i++) {
+            int left = 0;
+            int right = sortedTarget.length - 1;
+            int middle = (left + right) / 2;
+            while (left <= right) {
+                middle = (left + right) / 2;
+                if (sortedTarget[middle] == target[i]) {
+                    break;
+                } else if (sortedTarget[middle] < target[i]) {
+                    left = middle + 1;
+                } else if (sortedTarget[middle] > target[i]) {
+                    right = middle - 1;
+                }
+            }
+            result[i] = middle;
         }
 
         return result;
--- src/test/java/book/Chapter6ReTest.java
+++ src/test/java/book/Chapter6ReTest.java
@@ -14,4 +14,14 @@ class Chapter6ReTest {
         System.out.println();
         System.out.println(Arrays.toString(result));
     }
+
+    @Test
+    void coordinateCompressionRe() {
+        Chapter6ReTry chapter6ReTry = new Chapter6ReTry();
+        int[] result = chapter6ReTry.coordinateCompressionRe(new int[]{12, 43, 7, 15, 9});
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(Arrays.toString(result));
+    }
 }
