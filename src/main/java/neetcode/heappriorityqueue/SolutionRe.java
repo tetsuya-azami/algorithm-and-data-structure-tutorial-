64e19ea48ef5e3889748e0d2f078cc9b2471d9bf
tetsuya-azami <zqianjian35@gmail.com>
2024-12-07 08:44:57 +0900

    refactor kClosest

--- src/main/java/neetcode/heappriorityqueue/SolutionRe.java
+++ src/main/java/neetcode/heappriorityqueue/SolutionRe.java
@@ -1,16 +1,18 @@
 package neetcode.heappriorityqueue;
 
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class SolutionRe {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair<Integer, int[]>> queue = new PriorityQueue<>(k, Comparator.comparingInt(e -> e.key));
+        PriorityQueue<Pair<Integer, int[]>> queue = new PriorityQueue<>(k, (e1, e2) -> e2.key - e1.key);
         for (int i = 0; i < points.length; i++) {
             int[] point = points[i];
             int distance = point[0] * point[0] + point[1] * point[1];
             queue.offer(new Pair<>(distance, point));
+            if (queue.size() > k) {
+                queue.poll();
+            }
         }
 
         int[][] result = new int[k][2];
