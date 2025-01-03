fe839fed1754aa4637747a3396059687081b32fd
tetsuya-azami <zqianjian35@gmail.com>
2025-01-04 08:28:07 +0900

    refactor search

--- src/main/java/neetcode/binarysearch/SolutionReRe.java
+++ src/main/java/neetcode/binarysearch/SolutionReRe.java
@@ -96,20 +96,20 @@ public class SolutionReRe {
             int mid = l + (r - l) / 2;
             if (target == nums[mid]) return mid;
             if (nums[l] <= nums[mid]) {
-                if (target < nums[l] || target > nums[mid]) {
-                    l = mid + 1;
-                } else {
+                if (nums[l] <= target && target < nums[mid]) {
                     r = mid - 1;
+                } else {
+                    l = mid + 1;
                 }
-            } else { // midがright portion
-                if (target < nums[mid] || target > nums[r]) {
-                    r = mid - 1;
-                } else {
+            } else {
+                if (nums[mid] < target && target <= nums[r]) {
                     l = mid + 1;
+                } else {
+                    r = mid - 1;
                 }
             }
         }
-        
+
         return -1;
     }
 }
