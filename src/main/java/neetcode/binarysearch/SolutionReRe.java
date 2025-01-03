35e40aa2be7deefecc24393899796bdc411b5adf
tetsuya-azami <zqianjian35@gmail.com>
2025-01-04 08:21:11 +0900

    solve search

--- src/main/java/neetcode/binarysearch/SolutionReRe.java
+++ src/main/java/neetcode/binarysearch/SolutionReRe.java
@@ -88,4 +88,28 @@ public class SolutionReRe {
 
         return k;
     }
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (target == nums[mid]) return mid;
+            if (nums[l] <= nums[mid]) {
+                if (target < nums[l] || target > nums[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else { // midがright portion
+                if (target < nums[mid] || target > nums[r]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
 }
--- src/test/java/neetcode/binarysearch/SolutionReReTest.java
+++ src/test/java/neetcode/binarysearch/SolutionReReTest.java
@@ -112,4 +112,16 @@ class SolutionReReTest {
         System.out.println();
         System.out.println(result);
     }
+
+    @Test
+    public void search() {
+        int[] nums = new int[]{-1, 0, 2, 4, 6, 8};
+        int target = 4;
+        Solution solution = new Solution();
+        int result = solution.search(nums, target);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println(result);
+    }
 }
