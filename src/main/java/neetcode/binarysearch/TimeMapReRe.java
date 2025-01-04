fcc223a7335252c54098b6659e4ce921a1ea58c4
tetsuya-azami <zqianjian35@gmail.com>
2025-01-04 09:53:08 +0900

    refactor TimeMapReRe

--- src/main/java/neetcode/binarysearch/TimeMapReRe.java
+++ src/main/java/neetcode/binarysearch/TimeMapReRe.java
@@ -19,23 +19,22 @@ public class TimeMapReRe {
     }
 
     public String get(String key, int timestamp) {
-        List<Pair<Integer, String>> pairs = map.get(key);
-        if (pairs == null || pairs.isEmpty()) return "";
+        List<Pair<Integer, String>> pairs = map.getOrDefault(key, new ArrayList<>());
 
         int l = 0;
         int r = pairs.size() - 1;
-        int result = -1;
+        String result = "";
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (pairs.get(mid).key <= timestamp) {
-                result = Math.max(result, mid);
+                result = pairs.get(mid).value;
                 l = mid + 1;
             } else {
                 r = mid - 1;
             }
         }
 
-        return result == -1 ? "" : pairs.get(result).value;
+        return result;
     }
 
     record Pair<K, V>(K key, V value) {
